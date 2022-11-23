46d97e7825425b33f62312987b0e3cc5b46e7326
santizdr <santizuleta11@gmail.com>
2022-11-23 22:34:00 +0100

    #11 fix: añadir url a booth en inglés

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -4,6 +4,7 @@ from .views import BoothViewIng, BoothViewESP, BoothViewALE
 
 urlpatterns = [
     path('<int:voting_id>/', BoothViewIng.as_view()),
+    path('<int:voting_id>/ing', BoothViewIng.as_view()),
     path('<int:voting_id>/esp', BoothViewESP.as_view()),
     path('<int:voting_id>/ale', BoothViewALE.as_view()),
 ]
