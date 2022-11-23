aa6a92475d414a86be04833dd728282c711678af
miggavmar <miggavmar@alum.us.es>
2022-11-23 20:58:28 +0100

    #11 feat: urls.py en español y alemán

--- decide/booth/urls.py
+++ decide/booth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BoothView
+from .views import BoothView, BoothViewESP, BoothViewALE
 
 
 urlpatterns = [
     path('<int:voting_id>/', BoothView.as_view()),
+    path('<int:voting_id>/esp', BoothViewESP.as_view()),
+    path('<int:voting_id>/ale', BoothViewALE.as_view()),
 ]
