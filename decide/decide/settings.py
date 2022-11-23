d749aea9fff82c0a693073a83519ac74f252ab2d
miggavmar <miggavmar@alum.us.es>
2022-11-23 11:30:32 +0100

    #10 fix: cambiada la DATABASES

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,6 +133,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/decide')
 
 
 # Password validation
