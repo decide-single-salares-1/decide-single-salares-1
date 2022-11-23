c07a9a57b1d2ad8a8cb6583163ad0205ea653ef6
miggavmar <miggavmar@alum.us.es>
2022-11-24 00:26:49 +0100

    #11 fix: cambios en visualizer

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -35,3 +35,19 @@ class VisualizerViewENG(TemplateView):
             raise Http404
 
         return context
+
+
+class VisualizerViewALE(TemplateView):
+    template_name = 'visualizer/visualizerale.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+
+        return context
