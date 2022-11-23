198dfeff5fe901d16f5fae47a41939885db3a158
elmodo10 <elmodo10@gmail.com>
2022-11-23 12:10:20 +0100

    #7 feat: creacion de graficas

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -3,6 +3,12 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
 from base import mods
 
 
@@ -10,12 +16,34 @@ class VisualizerView(TemplateView):
     template_name = 'visualizer/visualizer.html'
 
     def get_context_data(self, **kwargs):
+    
+        
         context = super().get_context_data(**kwargs)
         vid = kwargs.get('voting_id', 0)
 
         try:
             r = mods.get('voting', params={'id': vid})
+            
+            V=json.dumps(r[0])
             context['voting'] = json.dumps(r[0])
+            opci=[]
+            votos=[]
+            for v in r[0]["postproc"]: 
+                opci.append(v["option"]) 
+                votos.append(v["votes"]) 
+            fig, ax = plt.subplots()
+            #Colocamos una etiqueta en el eje Y
+            ax.set_ylabel('Votos')
+            #Colocamos una etiqueta en el eje X
+            ax.set_title('Cantidad de Votos por opcion')
+            #Creamos la grafica de barras utilizando 'paises' como eje X y 'ventas' como eje y.
+            plt.bar(opci, votos)
+            plt.savefig('barras_simple.png')
+
+            
+            
+        
+        
         except:
             raise Http404
 
