7ad943f15eedda201703616ef20607cc7a0cfa33
miggavmar <miggavmar@alum.us.es>
2022-11-24 00:26:15 +0100

    #11 fix: añadidos urls a visualizer

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import VisualizerView, VisualizerViewENG
+from .views import VisualizerView, VisualizerViewENG, VisualizerViewALE
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
+    path('<int:voting_id>/esp', VisualizerView.as_view()),
     path('<int:voting_id>/eng', VisualizerViewENG.as_view()),
+    path('<int:voting_id>/ale', VisualizerViewALE.as_view()),
 ]
