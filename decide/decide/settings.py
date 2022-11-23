8287db25cce7ddf23e69222f3fb4ad754e3b9a79
miggavmar <miggavmar@alum.us.es>
2022-11-23 13:01:11 +0100

    #10 fix: update settings ssl require

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -123,13 +123,9 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/decide',
-        conn_max_age=600    
-    )
-}
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/decide')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
