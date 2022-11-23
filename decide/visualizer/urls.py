e98b280c651cf65c86d605bd74106e8e0616d367
miggavmar <miggavmar@alum.us.es>
2022-11-23 21:15:17 +0100

    #9 feat: urls.py y views.py de visualizer

--- decide/visualizer/urls.py
+++ decide/visualizer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import VisualizerView
+from .views import VisualizerView, VisualizerViewENG
 
 
 urlpatterns = [
     path('<int:voting_id>/', VisualizerView.as_view()),
+    path('<int:voting_id>/eng', VisualizerViewENG.as_view()),
 ]
--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -20,3 +20,18 @@ class VisualizerView(TemplateView):
             raise Http404
 
         return context
+
+class VisualizerViewENG(TemplateView):
+    template_name = 'visualizer/visualizereng.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        vid = kwargs.get('voting_id', 0)
+
+        try:
+            r = mods.get('voting', params={'id': vid})
+            context['voting'] = json.dumps(r[0])
+        except:
+            raise Http404
+
+        return context
