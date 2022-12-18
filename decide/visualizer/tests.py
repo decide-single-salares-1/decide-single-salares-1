2ffcb7a4d27157800fca9dc35416d0b207cfe573
elmodo10 <elmodo10@gmail.com>
2022-12-18 02:57:04 +0100

    #25 test: Done

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -11,101 +11,101 @@ import os
 
 class VisualizerTestCase(BaseTestCase):
     
-    # def test_check_funciona_la_pagina(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
+    def test_check_funciona_la_pagina(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
         
-    #     assert driver.find_element(By.CSS_SELECTOR, ".navbar-brand").text == "Decide, una app para sus votaciones y resultados"
+        assert driver.find_element(By.CSS_SELECTOR, ".navbar-brand").text == "Decide, una app para sus votaciones y resultados"
 
         
-    # def test_check_funcionan_estilos(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     color_h1 = Color.from_string(driver.find_element(By.CSS_SELECTOR, ".heading").value_of_css_property('background-color'))
+    def test_check_funcionan_estilos(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        color_h1 = Color.from_string(driver.find_element(By.CSS_SELECTOR, ".heading").value_of_css_property('background-color'))
         
-    #     assert color_h1.rgb == 'rgb(19, 136, 190)'
+        assert color_h1.rgb == 'rgb(19, 136, 190)'
 
         
-    # def test_check_aparece_boton_funciones_accesibilidad(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
+    def test_check_aparece_boton_funciones_accesibilidad(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
         
-    #     assert driver.find_element(By.ID, "boton").text == "Opciones de accesibilidad"
+        assert driver.find_element(By.ID, "boton").text == "Opciones de accesibilidad"
 
         
-    # def test_check_aparecen_opciones_accesibilidad(self):	
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     driver.find_element(By.ID, "boton").click()
-    #     driver.find_element(By.ID, "boton").click()
+    def test_check_aparecen_opciones_accesibilidad(self):	
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        driver.find_element(By.ID, "boton").click()
+        driver.find_element(By.ID, "boton").click()
 
-    #     assert driver.find_element(By.CSS_SELECTOR, "#acc > h3").text == "Accesibilidad"
+        assert driver.find_element(By.CSS_SELECTOR, "#acc > h3").text == "Accesibilidad"
        
     
-    # def test_check_modo_oscuro(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     driver.find_element(By.ID, "boton").click()
-    #     driver.find_element(By.ID, "boton").click()       
-    #     modo_oscuro = Color.from_string(driver.find_element(By.CSS_SELECTOR, "body").value_of_css_property('background'))
+    def test_check_modo_oscuro(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        driver.find_element(By.ID, "boton").click()
+        driver.find_element(By.ID, "boton").click()       
+        modo_oscuro = Color.from_string(driver.find_element(By.CSS_SELECTOR, "body").value_of_css_property('background'))
         
-    #     assert modo_oscuro.hex == '#ebf2f7'
+        assert modo_oscuro.hex == '#ebf2f7'
         
         
-    # def test_check_modo_alto_contraste(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     driver.find_element(By.ID, "boton").click()
-    #     driver.find_element(By.ID, "boton").click()
-    #     driver.find_element(By.ID, "switch3").click()
-    #     color_tabla= Color.from_string(driver.find_element(By.CSS_SELECTOR, "thead th:nth-child(1)").value_of_css_property('color'))
+    def test_check_modo_alto_contraste(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        driver.find_element(By.ID, "boton").click()
+        driver.find_element(By.ID, "boton").click()
+        driver.find_element(By.ID, "switch3").click()
+        color_tabla= Color.from_string(driver.find_element(By.CSS_SELECTOR, "thead th:nth-child(1)").value_of_css_property('color'))
         
-    #     assert color_tabla.rgba == 'rgba(0, 0, 0, 1)'   
+        assert color_tabla.rgba == 'rgba(0, 0, 0, 1)'   
     
-    # def test_creacion_de_graficas_lineas(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     #le damos la ruta de la imagen que se crea con las barras por lo que si existe es que se crean las barras
-    #     ruta_imagen = "barras_simple.png"
+    def test_creacion_de_graficas_lineas(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        #le damos la ruta de la imagen que se crea con las barras por lo que si existe es que se crean las barras
+        ruta_imagen = "barras_simple.png"
         
-    #     # Comprueba si la imagen existe en la carpeta especificada tras visitar el visualizer
-    #     assert os.path.exists(ruta_imagen)
+        # Comprueba si la imagen existe en la carpeta especificada tras visitar el visualizer
+        assert os.path.exists(ruta_imagen)
 
-    # def test_creacion_de_graficas_pastel(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     #le damos la ruta de la imagen que se crea con las barras por lo que si existe es que se crean las barras
-    #     ruta_imagen = "pie_simple.png"
+    def test_creacion_de_graficas_pastel(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        #le damos la ruta de la imagen que se crea con las barras por lo que si existe es que se crean las barras
+        ruta_imagen = "pie_simple.png"
         
-    #     # Comprueba si la imagen existe en la carpeta especificada tras visitar el visualizer
-    #     assert os.path.exists(ruta_imagen)
+        # Comprueba si la imagen existe en la carpeta especificada tras visitar el visualizer
+        assert os.path.exists(ruta_imagen)
         
-    # def test_creacion_de_PDF(self):
-    #     options = webdriver.FirefoxOptions()
-    #     options.headless = True
-    #     driver = webdriver.Firefox(options=options)
-    #     driver.get("http://localhost:8000/visualizer/1/")
-    #     #le damos la ruta de la pdf que se crea con las barras por lo que si existe es que se crean las barras
-    #     ruta_pdf = "barras.pdf"
+    def test_creacion_de_PDF(self):
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        driver = webdriver.Firefox(options=options)
+        driver.get("http://localhost:8000/visualizer/1/")
+        #le damos la ruta de la pdf que se crea con las barras por lo que si existe es que se crean las barras
+        ruta_pdf = "barras.pdf"
         
-    #     # Comprueba si la pdf existe en la carpeta especificada tras visitar el visualizer
-    #     assert os.path.exists(ruta_pdf)
+        # Comprueba si la pdf existe en la carpeta especificada tras visitar el visualizer
+        assert os.path.exists(ruta_pdf)
         
     
     def test_send_photo_telegrambot(self):
